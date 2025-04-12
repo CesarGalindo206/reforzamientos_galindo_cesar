7c59add628aad024a3391fd30cf24868c7efd4c5
Cesar Galindo <astraeayiyi@gmail.com>
2025-04-12 18:20:29 -0500

    reforzamiento 5 añadido

---
+++ reforzamiento05/test_01.py
@@ -0,0 +1,18 @@
+"""
+Exepciones
+
+1. Crear una función la cual tendrá una excepción para que tu programa
+no se bloquee donde se imprime un mensaje al usuario
+la causa y/o solución (Pedir nombre por consola), solo aceptará string como input:
+Si ingresa un entero, crear una excepción
+La salida de función tendrá que tener el siguiente mensaje:
+
+ "¡Hola NOMBRE!, hoy estamos DÍA de MES del AÑO"
+
+ Ejemplo:
+# ¡Hola Leonardo!, hoy estamos 04 de Mayo del 204
+
+La función también deberá captar la fecha actual, de allí pasará a
+tomar solo los datos que necesita para el examen final
+
+"""
---
+++ reforzamiento05/test_04.py
@@ -0,0 +1,12 @@
+"""
+Módulos
+
+4. Creando un archivo principal donde llamará a un módulo (operaciones.py)
+el cuál contendrá las siguientes funciones:
+
+-La primera función cargará a 3 números enteros que pedirá al usuario
+que ingrese por consola un valor.
+-La segunda función solamente sumará dos valores.
+-Desde el archivo principal importar al módulo y sumar dos valores
+usando las funciones anteriormente creadas en el módulo.
+"""
